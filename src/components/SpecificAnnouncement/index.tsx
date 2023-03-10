a27d442fa78c7763bdbce7f2543e9f5d8e098be4
Thiago <thiagocezar32@gmail.com>
2023-03-10 07:50:19 -0300

    :bug: updating Card

--- src/components/SpecificAnnouncement/index.tsx
+++ src/components/SpecificAnnouncement/index.tsx
@@ -12,97 +12,110 @@ const SpecificAnnouncement = () => {
 
   return (
     <ContainerSpecificAnnouncement>
-      <section className='data-page'>
-        <div className='container-main'>
-          <div className='main-aside'>
+      <section className="data-page">
+        <article className="container-main">
+          <div className="main-aside">
             <main>
               <figure>
-                <img src={vehicleSpecific?.cover_image} alt={vehicleSpecific?.cover_image} />
+                <img
+                  src={vehicleSpecific?.cover_image}
+                  alt={vehicleSpecific?.cover_image}
+                />
               </figure>
-              <article className='data-car'>
+              <article className="data-car">
                 <h2>{vehicleSpecific?.title}</h2>
-                <div className='date-km-value'>
-                  <div className='date-km'>
+                <div className="date-km-value">
+                  <div className="date-km">
                     <span>{vehicleSpecific?.year}</span>
                     <span>{vehicleSpecific?.mileage} KM</span>
                   </div>
-                  <span className='value'>R$ {vehicleSpecific?.price},00</span>
+                  <span className="value">R$ {vehicleSpecific?.price},00</span>
                 </div>
                 <button>Comprar</button>
               </article>
-              <article className='description-box'>
+              <article className="description-box">
                 <h2>Descrição</h2>
                 <p>{vehicleSpecific?.bio}</p>
               </article>
             </main>
             <aside>
-              <div className='carImg-box'>
+              <div className="carImg-box">
                 <label>Fotos</label>
                 <ul>
-                  {vehicleSpecific?.images.map((el) => (
-                    <figure key={el.id} onClick={()=> {
-                      setImageToModal(el.image)
-                      setImageModal(!imageModal)
-                    }}>
+                  {vehicleSpecific?.images?.map((el) => (
+                    <figure
+                      key={el.id}
+                      onClick={() => {
+                        setImageToModal(el.image);
+                        setImageModal(!imageModal);
+                      }}
+                    >
                       <img src={el.image} />
                     </figure>
                   ))}
                 </ul>
               </div>
-              <div className='adversiter-box'>
+              <div className="adversiter-box">
                 <h2>
-                  {vehicleSpecific?.user.name.split(" ")[0].substring(0,1).toLocaleUpperCase()}
-                  {vehicleSpecific?.user.name.split(" ")[1] && 
-                    (
-                      vehicleSpecific.user.name.split(" ")[1].length > 3 ?
-                      vehicleSpecific.user.name.split(" ")[1].substring(0,1).toLocaleUpperCase() :
-                      (vehicleSpecific.user.name.split(" ")[2] && 
-                        vehicleSpecific.user.name.split(" ")[2].substring(0,1).toLocaleUpperCase()
-                      )
-                    )
-                  }
+                  {vehicleSpecific?.user.name
+                    .split(" ")[0]
+                    .substring(0, 1)
+                    .toLocaleUpperCase()}
+                  {vehicleSpecific?.user.name.split(" ")[1] &&
+                    (vehicleSpecific.user.name.split(" ")[1].length > 3
+                      ? vehicleSpecific.user.name
+                          .split(" ")[1]
+                          .substring(0, 1)
+                          .toLocaleUpperCase()
+                      : vehicleSpecific.user.name.split(" ")[2] &&
+                        vehicleSpecific.user.name
+                          .split(" ")[2]
+                          .substring(0, 1)
+                          .toLocaleUpperCase())}
                 </h2>
                 <h3>
                   {vehicleSpecific?.user.name.split(" ")[0]}
                   {vehicleSpecific?.user.name.split(" ")[1]}
                 </h3>
                 <p>{vehicleSpecific?.user.bio}</p>
-                <button onClick={() => {
-                  navigate("advertiser", {replace: true})
-                }}>Ver todos anúncios</button>
+                <button
+                  onClick={() => {
+                    navigate("advertiser", { replace: true });
+                  }}
+                >
+                  Ver todos anúncios
+                </button>
               </div>
             </aside>
           </div>
           <ContainerComment>
-            <div className='comment-list'>
+            <div className="comment-list">
               <label>Comentários</label>
               <ul>
-                {
-                  commentsAd.length > 0 
-                  ?
+                {commentsAd.length > 0 ? (
                   commentsAd?.map((comment) => (
-                    <Comment 
+                    <Comment
                       key={comment.id}
                       userName={comment.user.name}
                       date={comment.created_at}
                       comment={comment.comment}
                     />
                   ))
-                  :
-                  <div className='no-comments'>
+                ) : (
+                  <div className="no-comments">
                     <h1>Sem comentários</h1>
                   </div>
-                }
+                )}
               </ul>
             </div>
-            <div className='create-comment'>
-              <CreateComment/>
+            <div className="create-comment">
+              <CreateComment />
             </div>
           </ContainerComment>
-        </div>
+        </article>
       </section>
     </ContainerSpecificAnnouncement>
-  )
+  );
 }
 
 export default SpecificAnnouncement
